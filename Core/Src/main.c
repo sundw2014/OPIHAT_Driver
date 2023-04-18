8da0cbda7d6b1c9ffbef21c2ccc2cf6d451366f8
Dawei Sun <sundw2014@gmail.com>
2023-04-18 17:19:16 -0500

    v1.0

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -127,8 +127,20 @@ int main(void)
   start_battery_monitor();
   start_cmd_receive();
   HAL_TIM_Base_Start(&htim1);
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_3);
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_4);
   HAL_TIM_Base_Start(&htim2);
+  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_2);
+  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_3);
+  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_4);
   HAL_TIM_Base_Start(&htim4);
+  HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_2);
+  HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_3);
+  HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_4);
   HAL_TIM_Base_Start_IT(&htim5);
   /* USER CODE END 2 */
 
--- Core/Src/motor.c
+++ Core/Src/motor.c
@@ -14,31 +14,31 @@ void set_motor_speed(int motor_ID, int16_t speed){
   }
   switch(motor_ID){
     case 1:
-      HAL_GPIO_WritePin(GPIOC, GPIO_PIN_1, IN1);
-      HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, IN2);
-      (&htim2)->Instance->CCR1 = ABS(speed);
-      break;
-    case 2:
-      HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2, IN1);
-      HAL_GPIO_WritePin(GPIOC, GPIO_PIN_3, IN2);
-      (&htim2)->Instance->CCR2 = ABS(speed);
-      break;
-    case 3:
-      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, IN1);
-      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, IN2);
-      (&htim2)->Instance->CCR3 = ABS(speed);
-      break;
-    case 4:
       HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, IN1);
       HAL_GPIO_WritePin(GPIOB, GPIO_PIN_1, IN2);
-      (&htim2)->Instance->CCR4 = ABS(speed);
+      __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_4, ABS(speed));
+      break;
+    case 2:
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, IN1);
+      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, IN2);
+      __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_3, ABS(speed));
+      break;
+    case 3:
+      HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0, IN1);
+      HAL_GPIO_WritePin(GPIOC, GPIO_PIN_1, IN2);
+      __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, ABS(speed));
+      break;
+    case 4:
+      HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2, IN1);
+      HAL_GPIO_WritePin(GPIOC, GPIO_PIN_3, IN2);
+      __HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_2, ABS(speed));
       break;
   }
 }
 
 void set_motors(int16_t *motors){
-    printf("M: %d %d %d %d\r\n", motors[0], motors[1], motors[2], motors[3]);
+//    printf("M: %d %d %d %d\r\n", motors[0], motors[1], motors[2], motors[3]);
     for(int i=0;i<4;i++){
-        set_motor_speed(i, motors[i]);
+        set_motor_speed(i+1, motors[i]);
     }
 }
